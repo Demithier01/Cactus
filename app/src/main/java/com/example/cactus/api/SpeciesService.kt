84e9fbed816e816f90ab9dea87ece9a42db3c0fc
dreamii <darika.ko@ku.th>
2024-01-25 15:54:03 +0700

    update

--- app/src/main/java/com/example/cactus/api/SpeciesService.kt
+++ app/src/main/java/com/example/cactus/api/SpeciesService.kt
@@ -10,16 +10,16 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface SpeciesService {
-    @GET("/cactus/.json")
+    @GET("/backup/cactus/.json")
     fun getSpecies(): Call<Map<String, SpeciesItem>>
 
-    @PATCH("/cactus/{id}.json")
+    @PATCH("/backup/cactus/{id}.json")
     fun updateSpecies(@Path("id") id: String, @Body speciesItem: SpeciesItem): Call<SpeciesItem>
 
-    @POST("/cactus/.json")
+    @POST("/backup/cactus/.json")
     fun createSpecies(@Body speciesItem: SpeciesItem): Call<SpeciesItem>
 
-    @DELETE("/cactus/{id}.json")
+    @DELETE("/backup/cactus/{id}.json")
     fun deleteSpecies(@Path("id")id: String): Call<SpeciesItem>
 }
 
