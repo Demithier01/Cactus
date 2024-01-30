70065320f51db2e4933014ad2271628b115d77f1
dreamii <darika.ko@ku.th>
2024-01-30 11:35:30 +0700

    Add a Detail page and an AddData page for adding information(update)

--- app/src/main/java/com/example/cactus/DetailFragment.kt
+++ app/src/main/java/com/example/cactus/DetailFragment.kt
@@ -14,7 +14,7 @@ class DetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
        binding = FragmentDetailBinding.inflate(inflater,container,false)
        return binding.root
     }
